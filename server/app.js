23340c728a1a3fbc8b51161e38b87786fea99c68
stellaCF <stellachommy01@gmail.com>
2025-05-16 10:58:05 +0100

    Configure CORS to allow requests from localhost with credentials

--- server/app.js
+++ server/app.js
@@ -11,7 +11,10 @@ const emptyReqBody = require('./src/middlewares/emptyReqBody');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin: "http://localhost:5173",
+   credentials: true,
+ }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
