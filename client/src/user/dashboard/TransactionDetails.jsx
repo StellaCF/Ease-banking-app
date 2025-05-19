80b5e7d9ff3736e1590f803079d5e08ebc116a0a
stellaCF <stellachommy01@gmail.com>
2025-05-19 23:51:46 +0100

    Enhance TransactionDetails component layout

--- client/src/user/dashboard/TransactionDetails.jsx
+++ client/src/user/dashboard/TransactionDetails.jsx
@@ -16,24 +16,27 @@ const TransactionDetails = () => {
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
       <main className="ml-64 flex-1 p-8 space-y-8">
-        <button onClick={() => navigate(-1)} className="text-white bg-[#02487F] hover:bg-[#1384AB] rounded-md p-2">
-          ←
-        </button>
-
-        <div className="bg-white p-6 rounded-xl shadow-md">
-          <h2 className="text-2xl font-bold mb-4 text-[#02487F]">Transaction Details</h2>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <Detail label="Type" value={txn.type} />
-            <Detail label="Amount" value={`₦${txn.amount.toLocaleString()}`} />
-            <Detail label="Status" value={txn.status} />
-            <Detail label="Date" value={date} />
-            <Detail label="Time" value={time} />
-            <Detail label="Sender Name" value={txn.senderName || "N/A"} />
-            <Detail label="Sender Account Number" value={txn.senderAccount || "N/A"} />
-            <Detail label="Receiver Name" value={txn.receiverName || "N/A"} />
-            <Detail label="Receiver Account Number" value={txn.receiverAccount || "N/A"} />
-            <Detail label="Source" value={txn.source} />
+        <div className="flex justify-between items-center">
+          {/* <h2 className="text-2xl font-bold text-[#02487F]">{txn.type}</h2> */}
+          <button onClick={() => navigate(-1)} className="text-white bg-[#02487F] hover:bg-[#1384AB] rounded-md px-3 py-1">
+            ←
+          </button>
+        </div>
+        <div className="w-5/12 mx-auto rounded-xl shadow-md p-1 bg-linear-to-r from-[#02487F] to-[#1384AB]">
+          <div className="bg-[#ffffffc5] px-8 py-6 w-full rounded-xl">
+            <div className="w-30 h-30 mx-auto flex justify-center items-center bg-green-800 text-white font-bold text-sm rounded-full">{txn.status.toUpperCase()}</div>
+            <div className="grid grid-cols-1 mt-8 gap-3">
+              <Detail label="Type" value={txn.type} />
+              <Detail label="Amount" value={`₦${txn.amount.toLocaleString()}`} />
+              <Detail label="Status" value={txn.status} />
+              <Detail label="Date" value={date} />
+              <Detail label="Time" value={time} />
+              <Detail label="Sender Name" value={txn.senderName || "N/A"} />
+              <Detail label="Sender Account Number" value={txn.senderAccount || "N/A"} />
+              <Detail label="Receiver Name" value={txn.receiverName || "N/A"} />
+              <Detail label="Receiver Account Number" value={txn.receiverAccount || "N/A"} />
+              <Detail label="Transaction ID" value={txn.id} />
+            </div>
           </div>
         </div>
       </main>
@@ -42,8 +45,8 @@ const TransactionDetails = () => {
 };
 
 const Detail = ({ label, value }) => (
-  <div>
-    <p className="text-sm text-gray-500">{label}</p>
+  <div className="flex justify-between">
+    <p className="text-md text-gray-700">{label}:</p>
     <p className="text-lg font-semibold text-gray-800">{value}</p>
   </div>
 );
