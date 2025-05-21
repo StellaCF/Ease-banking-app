7e43195d69b92b5c0d28275b4ab1d0f22a5a74e0
stellaCF <stellachommy01@gmail.com>
2025-05-21 15:21:52 +0100

    Update CORS origin to production URL

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const emptyReqBody = require('./src/middlewares/emptyReqBody');
 const app = express();
 
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: "https://easebank-drab.vercel.app",
    credentials: true,
  }));
 app.use(express.json());
