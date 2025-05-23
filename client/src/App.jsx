56159a5e63333bcc54195d3f422cf85e231f863d
stellaCF <stellachommy01@gmail.com>
2025-05-23 01:28:03 +0100

    Wrap App component with BrowserRouter for routing support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage  from './user/LandingPage';
 import Dashboard from './user/dashboard/Dashboard';
 import SignUp from './auth/SignUp';
@@ -41,27 +41,25 @@ const App = () => {
   }, [navigate]);
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path ="/dashboard" element={ <Dashboard /> } />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/forgetPassword" element={<ForgetPassword />} />
-          <Route path="/verifyPassword" element={<VerifyPassword />} />
-          <Route path="/resetPassword" element={<ResetPassword />} />
-          <Route path="/transactionPin" element={<TransactionPin />} />
-          <Route path="/depositPage" element={<DepositPage />} />
-          <Route path="/withdrawPage" element={<WithdrawPage />} />
-          <Route path="/transferPage" element={<TransferPage />} />
-          <Route path="/savePage" element={<SavePage />} />
-          <Route path="/loanPage" element={<LoanPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/transactiondetails" element={<TransactionDetails />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path ="/dashboard" element={ <Dashboard /> } />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgetPassword" element={<ForgetPassword />} />
+        <Route path="/verifyPassword" element={<VerifyPassword />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/transactionPin" element={<TransactionPin />} />
+        <Route path="/depositPage" element={<DepositPage />} />
+        <Route path="/withdrawPage" element={<WithdrawPage />} />
+        <Route path="/transferPage" element={<TransferPage />} />
+        <Route path="/savePage" element={<SavePage />} />
+        <Route path="/loanPage" element={<LoanPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/transactiondetails" element={<TransactionDetails />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
       <ToastContainer/>
     </div>
   );
